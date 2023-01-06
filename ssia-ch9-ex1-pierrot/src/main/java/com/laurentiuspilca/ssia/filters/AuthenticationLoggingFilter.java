d9fa06c2286bfb3b69514a5c40942ca36ea2cbe4
PierreSQS <nomo_messina@web.de>
2023-01-06 17:35:01 +0100

    Chap9-Filters-Pierrot: added Lombok

--- ssia-ch9-ex1-pierrot/src/main/java/com/laurentiuspilca/ssia/filters/AuthenticationLoggingFilter.java
+++ ssia-ch9-ex1-pierrot/src/main/java/com/laurentiuspilca/ssia/filters/AuthenticationLoggingFilter.java
@@ -1,23 +1,21 @@
 package com.laurentiuspilca.ssia.filters;
 
 
-import jakarta.servlet.Filter;
-
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
-import java.util.logging.Logger;
 
-    public class AuthenticationLoggingFilter implements Filter {
 
-    private final Logger logger =
-            Logger.getLogger(AuthenticationLoggingFilter.class.getName());
+@Slf4j
+public class AuthenticationLoggingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         var httpRequest = (HttpServletRequest) request;
         String requestId = httpRequest.getHeader("Request-Id");
-        logger.info("Successfully authenticated request with id " +  requestId);
+        log.info("Successfully authenticated request with id {}" ,requestId);
         filterChain.doFilter(request, response);
     }
 }
