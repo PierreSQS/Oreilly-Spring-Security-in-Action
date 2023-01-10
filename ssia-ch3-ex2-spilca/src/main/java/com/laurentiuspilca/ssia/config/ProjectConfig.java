be42e5540ad82c441d0eafe823580b1ab7ced134
PierreSQS <nomo_messina@web.de>
2023-01-10 19:23:11 +0100

    Chap3-Managing Users-Spilca-Pierrot-Trials: accessing H2 Console with SB 2.3.0.REL

--- ssia-ch3-ex2-spilca/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch3-ex2-spilca/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,6 +13,16 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().ignoringAntMatchers("/h2-console/**")
+    .and()
+            .authorizeRequests()
+            .antMatchers("/h2-console/**").permitAll();
+
+    http.headers().frameOptions().sameOrigin();
+  }
 
   @Bean
   public UserDetailsService userDetailsService(DataSource dataSource) {
