7230616441f7385fd125d037bba8008d898d04eb
PierreSQS <nomo_messina@web.de>
2023-01-11 00:16:20 +0100

    Chap5 Implementing authentication-Pierrot: upgraded SB to 3.0.1, Java to 17 and maven to 3.8.6

--- ssia-ch5-ex1-pierrot/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch5-ex1-pierrot/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -1,11 +1,7 @@
 package com.laurentiuspilca.ssia.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,10 +10,7 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import javax.sql.DataSource;
 
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private AuthenticationProvider authenticationProvider;
+public class ProjectConfig {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
@@ -29,8 +22,4 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider);
-    }
 }
