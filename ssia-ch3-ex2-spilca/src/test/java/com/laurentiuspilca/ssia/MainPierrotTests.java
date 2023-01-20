85033c87994190e9143116b1ff1ed4292151a697
PierreSQS <nomo_messina@web.de>
2023-01-21 00:41:23 +0100

    Chap3-Managing Users-Spilca-Pierrot-Trials: added a new test and attempted to test UserService

--- ssia-ch3-ex2-spilca/src/test/java/com/laurentiuspilca/ssia/MainPierrotTests.java
+++ ssia-ch3-ex2-spilca/src/test/java/com/laurentiuspilca/ssia/MainPierrotTests.java
@@ -1,21 +1,17 @@
 package com.laurentiuspilca.ssia;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class MainTests {
+@WebMvcTest
+class MainPierrotTests {
 
     @Autowired
     MockMvc mvc;
@@ -31,16 +27,14 @@ class MainTests {
     @DisplayName("Test calling /hello endpoint authenticated returns ok.")
     void helloAuthenticated() throws Exception {
         mvc.perform(get("/hello")
-                .with(user("john")))
+                        .with(user("john")))
                 .andExpect(status().isOk());
     }
-
-    @Disabled("Doesn't work with SB2.3.0 but work in SB3.0.1")
     @Test
-    @WithUserDetails("john")
-    @DisplayName("Test calling /hello endpoint with UserDetailsService user exists.")
-    void helloUserServiceUserExists() throws Exception {
-        mvc.perform(get("/hello"))
+    @DisplayName("Test calling /hello endpoint authenticated user doesn't exists returns ok.")
+    void helloAuthenticatedUserNotExists() throws Exception {
+        mvc.perform(get("/hello")
+                        .with(user("johnny")))
                 .andExpect(status().isOk());
     }
 
