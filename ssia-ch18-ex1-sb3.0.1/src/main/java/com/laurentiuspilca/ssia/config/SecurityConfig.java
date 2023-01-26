337f22d1802f5cf2750e170c64927a7ae6270caf
PierreSQS <nomo_messina@web.de>
2023-01-26 16:20:16 +0100

    Chap18.3 Implementing the resource server-NOK: upgraded to SB3.0.1

---
+++ ssia-ch18-ex1-sb3.0.1/src/main/java/com/laurentiuspilca/ssia/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.laurentiuspilca.ssia.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${claim.aud}")
+    private String claimAud;
+
+    @Value("${jwkSetUri}")
+    private String urlJwk;
+
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.DELETE, "/**").hasAuthority("fitnessadmin")
+                .anyRequest().authenticated().and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        return http.build();
+    }
+
+    @Bean
+    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
+        return new SecurityEvaluationContextExtension();
+    }
+
+
+}
