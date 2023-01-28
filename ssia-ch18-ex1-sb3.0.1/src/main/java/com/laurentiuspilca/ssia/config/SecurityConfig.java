04cb0e2f6952df316efa3b42c6b65f93e11683d3
PierreSQS <nomo_messina@web.de>
2023-01-28 11:08:36 +0100

    Chap18.3 Implementing the resource server-NOK: set the Resource Server accurately

--- ssia-ch18-ex1-sb3.0.1/src/main/java/com/laurentiuspilca/ssia/config/SecurityConfig.java
+++ ssia-ch18-ex1-sb3.0.1/src/main/java/com/laurentiuspilca/ssia/config/SecurityConfig.java
@@ -25,7 +25,11 @@ public class SecurityConfig {
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .requestMatchers(HttpMethod.DELETE, "/**").hasAuthority("fitnessadmin")
-                .anyRequest().authenticated().and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                .anyRequest().authenticated()
+             .and()
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> jwt.jwkSetUri(urlJwk)
+                        ));
 
         return http.build();
     }
