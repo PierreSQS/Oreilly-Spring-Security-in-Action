6b4ac74fbf635448b08425fc3187e03af3952252
PierreSQS <nomo_messina@web.de>
2023-01-30 17:19:36 +0100

    Chap18.3 Implementing the resource server: fixed the Admin Authority

--- ssia-ch18-ex1-sb3.0.1/src/main/java/com/laurentiuspilca/ssia/config/SecurityConfig.java
+++ ssia-ch18-ex1-sb3.0.1/src/main/java/com/laurentiuspilca/ssia/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.DELETE, "/**").hasAuthority("fitnessadmin")
+                .requestMatchers(HttpMethod.DELETE, "/**").hasAuthority("SCOPE_fitnessadmin")
                 .anyRequest().authenticated()
              .and()
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
