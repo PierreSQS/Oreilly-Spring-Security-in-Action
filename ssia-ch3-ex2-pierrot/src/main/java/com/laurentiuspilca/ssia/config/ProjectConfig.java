84675c86b797ad41fbbe0c8e0dd619d56fbbd64c
PierreSQS <nomo_messina@web.de>
2023-03-27 13:57:16 +0200

    Chap3-Managing Users-Pierrot: alternative without PasswordEncoder factory

--- ssia-ch3-ex2-pierrot/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch3-ex2-pierrot/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -41,7 +41,7 @@ public class ProjectConfig {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    return new BCryptPasswordEncoder();
   }
 
 }
