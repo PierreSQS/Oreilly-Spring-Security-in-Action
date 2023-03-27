f8b0f64c86c38384494fbdc44cf6d41a59048a28
PierreSQS <nomo_messina@web.de>
2023-03-27 11:28:34 +0200

    Chap3-Managing Users-Pierrot-NOK: fixed the issue in Postman

--- ssia-ch3-ex2-pierrot/src/main/resources/data.sql
+++ ssia-ch3-ex2-pierrot/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO `spring`.`authorities` (username,authority) VALUES ('john', 'write');
-INSERT INTO `spring`.`users` (username,password,enabled) VALUES ('john', '12345', '1');
+INSERT INTO `spring`.`users` (username,password,enabled) VALUES ('john', '{bcrypt}$2a$10$euM3U5IlVWxSAGgeJT4zb.M/rp8TAjfITzXGg0zZWUzML.nYOEXMC', '1');
--- ssia-ch3-ex2-pierrot/src/main/resources/schema.sql
+++ ssia-ch3-ex2-pierrot/src/main/resources/schema.sql
@@ -3,7 +3,7 @@ create schema spring;
 CREATE TABLE IF NOT EXISTS `spring`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NULL,
-  `password` VARCHAR(45) NULL,
+  `password` VARCHAR(70) NULL,
   `enabled` INT NOT NULL,
   PRIMARY KEY (`id`));
 
