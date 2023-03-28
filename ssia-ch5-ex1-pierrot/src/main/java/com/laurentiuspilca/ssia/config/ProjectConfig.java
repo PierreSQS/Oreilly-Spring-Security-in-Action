87cd026c2efe5dd6eebcaa59673f122ebfb328a1
PierreSQS <nomo_messina@web.de>
2023-03-28 17:13:22 +0200

    Chap5.1 Understanding the AuthenticationProvider-Pierrot: introduced Bcrypt

--- ssia-ch5-ex1-pierrot/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch5-ex1-pierrot/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -3,7 +3,7 @@ package com.laurentiuspilca.ssia.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
@@ -19,7 +19,7 @@ public class ProjectConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
