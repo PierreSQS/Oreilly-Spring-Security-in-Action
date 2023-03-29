6335541f2a78035e41752696bca93a5b62c38f1a
PierreSQS <nomo_messina@web.de>
2023-03-29 14:13:55 +0200

    Chap5.1 Understanding the AuthenticationProvider-Pierrot: extended and optimized test

--- ssia-ch5-ex1-pierrot/src/test/java/com/laurentiuspilca/ssia/MainTests.java
+++ ssia-ch5-ex1-pierrot/src/test/java/com/laurentiuspilca/ssia/MainTests.java
@@ -6,31 +6,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MainTests {
+class MainTests {
 
     @Autowired
-    private MockMvc mvc;
+    MockMvc mvc;
 
     @Test
     @DisplayName("Test calling /hello endpoint without authentication returns unauthorized.")
-    public void helloUnauthenticated() throws Exception {
+    void helloUnauthenticated() throws Exception {
         mvc.perform(get("/hello"))
-                .andExpect(status().isUnauthorized());
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
     }
 
     @Test
     @DisplayName("Test calling /hello endpoint authenticated returns ok.")
     @WithMockUser
-    public void helloAuthenticated() throws Exception {
+    void helloAuthenticated() throws Exception {
         mvc.perform(get("/hello"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Test calling /hello endpoint authenticated with UserDetails returns ok.")
+    @WithUserDetails("john")
+    void helloAuthenticatedWithUserDetails() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello"))
+                .andDo(print());
     }
 
 }
