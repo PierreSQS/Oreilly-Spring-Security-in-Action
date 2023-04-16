3b8b2312fd92cb8c4fd68236e5c0a1256a10a68c
PierreSQS <nomo_messina@web.de>
2023-04-16 19:15:45 +0200

    Chap8.1 Using matcher methods-Pierrot-NOK: Removed the NoopEncoder

--- ssia-ch8-ex3-sb3.0.5-INT/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch8-ex3-sb3.0.5-INT/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -17,12 +17,12 @@ public class ProjectConfig {
         var manager = new InMemoryUserDetailsManager();
 
         var user1 = User.withUsername("john")
-                .password("12345")
+                .password("{bcrypt}$2a$10$i52AF2SwRAcQzYN9RmUO7.s3aZQoWeAhPGnVmOBx2pZq7oNBWn60i")//12345
                 .roles("ADMIN")
                 .build();
 
         var user2 = User.withUsername("jane")
-                .password("12345")
+                .password("{bcrypt}$2a$10$EphUjyTxPzzEUaZgrZzl/OvKKpxiIB4Qxbpfs4c.Kkle1pVRmLYZC")//12345
                 .roles("MANAGER")
                 .build();
 
@@ -34,7 +34,7 @@ public class ProjectConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     protected void configure(HttpSecurity http) throws Exception {
