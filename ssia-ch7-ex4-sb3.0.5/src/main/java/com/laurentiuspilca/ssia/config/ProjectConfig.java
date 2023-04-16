5ddc60043142decb3031fbc82b38a79a3dd6768b
PierreSQS <nomo_messina@web.de>
2023-04-16 12:11:11 +0200

    Chap7 Configuring authorization-Pierrot: upgraded SB to 3.0.5, Java to 17 and maven to 3.8.7

--- ssia-ch7-ex4-sb3.0.5/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch7-ex4-sb3.0.5/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -3,17 +3,16 @@ package com.laurentiuspilca.ssia.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
+public class ProjectConfig {
 
-    @Override
     @Bean
     public UserDetailsService userDetailsService() {
         var manager = new InMemoryUserDetailsManager();
@@ -39,12 +38,14 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.httpBasic();
 
 
         http.authorizeRequests()
                 .anyRequest().access("T(java.time.LocalTime).now().isAfter(T(java.time.LocalTime).of(12, 0))");
+
+        return http.build();
     }
 }
