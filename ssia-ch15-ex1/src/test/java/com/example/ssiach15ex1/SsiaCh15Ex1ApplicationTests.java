0a2d437887a825f2934c3352e6318a92ae9ef8cf
PierreSQS <nomo_messina@web.de>
2025-09-18 19:00:39 +0200

    Chap15.1-XXX: optimized test code

--- ssia-ch15-ex1/src/test/java/com/example/ssiach15ex1/SsiaCh15Ex1ApplicationTests.java
+++ ssia-ch15-ex1/src/test/java/com/example/ssiach15ex1/SsiaCh15Ex1ApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.ssiach15ex1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SsiaCh15Ex1ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+        assertThat(appCtx).isNotNull();
 	}
 
 }
